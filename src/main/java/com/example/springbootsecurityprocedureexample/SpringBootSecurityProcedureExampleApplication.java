68ebf83222f148140ad38f014a1a00b43b57d423
이승환 <lsh01087674117@gmail.com>
2022-04-11 16:22:24 +0900

    프로젝트 초기생성

---
+++ src/main/java/com/example/springbootsecurityprocedureexample/SpringBootSecurityProcedureExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurityprocedureexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityProcedureExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityProcedureExampleApplication.class, args);
+    }
+
+}
