2057092ce150d97599ceaee9b2e5edcc8455d415
이승환 <lsh01087674117@gmail.com>
2022-04-11 20:36:20 +0900

    UserDetails 구현한 UserDetailsVO 추가.

---
+++ src/main/java/com/example/springbootsecurityprocedureexample/app/UserDetailsVO.java
@@ -0,0 +1,52 @@
+package com.example.springbootsecurityprocedureexample.app;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * @author 이승환
+ * @since 2022-04-11
+ */
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsVO implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
