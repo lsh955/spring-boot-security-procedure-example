43727af4162b5304901b17f706a4545fd1ac441c
이승환 <lsh01087674117@gmail.com>
2022-04-11 17:48:10 +0900

    configure 메소드를 통해 정적 자원들에 대해서는 Security를 적용하지 않음을 추가

--- src/main/java/com/example/springbootsecurityprocedureexample/config/web/WebSecurityConfig.java
+++ src/main/java/com/example/springbootsecurityprocedureexample/config/web/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.springbootsecurityprocedureexample.config.web;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,5 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    
+    @Override
+    public void configure(WebSecurity web) {
+        // 정적 자원에 대해서는 Security 설정을 적용하지 않음
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+
 }
