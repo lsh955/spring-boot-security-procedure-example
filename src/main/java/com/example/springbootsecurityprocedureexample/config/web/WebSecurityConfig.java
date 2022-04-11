a1574aaf5120eee08c826262830c1c3863d2d9fa
이승환 <lsh01087674117@gmail.com>
2022-04-11 17:49:31 +0900

    비밀번호를 암호화하는 데 사용할 수 있는 메서드 추가.

--- src/main/java/com/example/springbootsecurityprocedureexample/config/web/WebSecurityConfig.java
+++ src/main/java/com/example/springbootsecurityprocedureexample/config/web/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.springbootsecurityprocedureexample.config.web;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -43,5 +45,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
