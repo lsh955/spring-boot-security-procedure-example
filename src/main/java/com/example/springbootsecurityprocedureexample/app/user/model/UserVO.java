9b96a416cb6998046e0fb611c3bb2a7a508f927a
이승환 <lsh01087674117@gmail.com>
2022-04-18 22:45:59 +0900

    UserVO 추가.

---
+++ src/main/java/com/example/springbootsecurityprocedureexample/app/user/model/UserVO.java
@@ -0,0 +1,44 @@
+package com.example.springbootsecurityprocedureexample.app.user.model;
+
+import com.example.springbootsecurityprocedureexample.app.common.model.CommonVO;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.Serializable;
+
+/**
+ * @author 이승환
+ * @since 2022-04-18
+ */
+@NoArgsConstructor
+@Entity
+@Table(name = "USER")
+@Getter
+public class UserVO extends CommonVO implements Serializable {
+
+    @Setter
+    @Column(nullable = false, unique = true, length = 50)
+    private String userEmail;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPw;
+
+    @Setter
+    @Column(nullable = false, length = 50)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Builder
+    public UserVO(String userEmail, String userPw){
+        this.userEmail = userEmail;
+        this.userPw = userPw;
+    }
+}
