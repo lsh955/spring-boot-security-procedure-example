8951a58fc1ff3914ab2fc70e9508c29ae4fcdf42
이승환 <lsh01087674117@gmail.com>
2022-04-18 23:04:54 +0900

    UserDetailsService 를 구현한 클래스 추가.

--- src/main/java/com/example/springbootsecurityprocedureexample/app/user/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/springbootsecurityprocedureexample/app/user/service/UserDetailsServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.springbootsecurityprocedureexample.app.user.service;
 
+import com.example.springbootsecurityprocedureexample.app.user.model.UserDetailsVO;
 import com.example.springbootsecurityprocedureexample.app.user.repository.UserRepository;
+import com.example.springbootsecurityprocedureexample.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 /**
  * @author 이승환
@@ -17,9 +20,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetailsVO loadUserByUsername(String userEmail) {
+        return userRepository.findByUserEmail(userEmail).map(u -> new UserDetailsVO(u, Collections.singleton(new SimpleGrantedAuthority(u.getRole().getValue())))).orElseThrow(() -> new UserNotFoundException(userEmail));
     }
 }
