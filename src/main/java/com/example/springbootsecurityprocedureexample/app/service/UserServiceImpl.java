2e114f7c907d4f594f4e4b90037682290212560f
이승환 <lsh01087674117@gmail.com>
2022-04-18 22:31:14 +0900

    UserServiceImpl 추가

---
+++ src/main/java/com/example/springbootsecurityprocedureexample/app/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.springbootsecurityprocedureexample.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 이승환
+ * @since 2022-04-18
+ */
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+}
